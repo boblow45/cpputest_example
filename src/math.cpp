5a5f2c39f19073ea9b466791518b555479a71777
Cillian O'Brien <cillianobrien01@gmail.com>
2020-08-27 02:20:48 +0100

    adding clang format file

--- src/math.cpp
+++ src/math.cpp
@@ -1,11 +1,9 @@
 #include "./../inc/math.hpp"
 
-uint32_t add(uint32_t x, uint32_t y)
-{
-    if (x == 0)
-    {
-        if (x == 0)
-            return y;
-    }
-    return x + y;
+uint32_t add(uint32_t x, uint32_t y) {
+	if(x == 0) {
+		if(x == 0)
+			return y;
+	}
+	return x + y;
 }
